6ad16d98d7030a432acd41ed086c5d6550b9e02c
cinna-codes <somethingfellipickeditup@gmail.com>
2020-10-23 21:18:23 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,22 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+        erb :index
+    end
+
+    post '/teams' do
+        @team = Team.new(params[:team][:name], params[:team][:motto])
+
+        heroes_array = params[:team][:members].map do |specs|
+            new_hero = Hero.new(specs[:name], specs[:power], specs[:bio])
+        end
+
+        @member1 = heroes_array[0]
+        @member2 = heroes_array[1]
+        @member3 = heroes_array[2]
+
+        erb :team
+    end
 
 end
