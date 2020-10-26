bd989cf472e1efccea2bc5ce55d3f8dc0bb220cf
github username <github email address>
2020-10-26 10:46:58 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,13 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+        erb :super_hero
+    end
 
+    post '/teams' do
+        @heros = params[:team]
+        @hero_members = params[:team][:member]
+        erb :team
+    end
 end
