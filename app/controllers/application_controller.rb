ec09bf95729027e36fe14a3690520aec933d5995
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-10-27 08:05:30 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,18 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+      erb :'../views/super_hero'
+    end
+    
+    post '/team' do
+      @team_name = params["team"]["name"]
+      @team_motto = params["team"]["motto"]
 
+      @member1 = params["team"]["members"][0]
+      @member2 = params["team"]["members"][1]
+      @member3 = params["team"]["members"][2]
+      
+      erb :'../views/team'
+    end
 end
