d7e43bfc295d26d9e43b9e5d5da35694732575d8
Kelin Field <kfield111@hotmail.com>
2020-10-27 02:58:59 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,13 @@ class App < Sinatra::Base
     end
 
     post '/teams' do
+      @team = Team.new(params[:team])
+
+      params[:team][:member].each do |temp|
+        Hero.new(temp)
+      end
+
+      @heros = Hero.all
 
       erb :team
     end
