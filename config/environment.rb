1d6a04215e6f6a6e02a096ffa53b3428fff15568
atencioluke <atencioluke@gmail.com>
2020-10-28 13:43:12 -0700

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,10 @@
 ENV['SINATRA_ENV'] ||= "development"
 
+# require 'require_all'
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
-require_all 'app'
+Dir["../app/models/"].each {|file| require file }
+Dir["../app/views/"].each {|file| require file }
+Dir["../app/controllers/"].each {|file| require file }
+# require_all 'app'
